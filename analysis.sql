709d6bcb32b8fc0eaca45fe4dd1e6a3c0275c78b
asdrasantana <asdra.andrade@gmail.com>
2026-06-18 16:24:53 -0300

    Create analysis.sql

---
+++ analysis.sql
@@ -0,0 +1,67 @@
+with olist as (
+select
+	date_trunc('month', od.order_purchase_timestamp) as month,
+	oi.product_id,
+	count(oi.order_id) as order_count,
+	avg(oi.price) as avg_price
+from olist_order_items_dataset as oi
+inner join olist_orders_dataset as od
+	on oi.order_id = od.order_id
+group by 
+	oi.product_id, 
+	date_trunc('month', od.order_purchase_timestamp)
+),
+product as (
+select 
+	p.product_id,
+	coalesce(cn.product_category_name_english, 'other / uncategorized') as product_category_name_english
+from olist_products_dataset as p
+left join olist_category_name_translation as cn
+	on p.product_category_name = cn.product_category_name
+),
+prev as (
+select
+	month,
+	product_id,
+	order_count,
+	avg_price,
+	lag(order_count) over 
+		(partition by product_id order by month) as previous_order_count,
+	lag(avg_price) over 
+		(partition by product_id order by month) as previous_avg_price
+from olist
+),
+percent as (
+select 
+	p.month,
+	p.product_id,
+	pd.product_category_name_english as product,
+	p.order_count,
+	p.avg_price,
+	p.previous_order_count,
+	p.previous_avg_price,
+	(p.order_count - nullif(p.previous_order_count, 0))/nullif(p.previous_order_count, 0) as percent_order_count,
+	(p.avg_price - nullif(p.previous_avg_price, 0))/nullif(p.previous_avg_price, 0) as percent_avg_price
+from prev as p
+left join product as pd
+	on p.product_id = pd.product_id
+)
+select
+	month,
+	product,
+	order_count,
+	avg_price,
+	previous_order_count,
+	previous_avg_price,
+	(percent_order_count)/ nullif(percent_avg_price, 0) as ped,
+	case
+		when abs(percent_order_count/nullif(percent_avg_price, 0)) > 1 then 'elastic'
+		when abs(percent_order_count/nullif(percent_avg_price, 0)) = 1 then 'unit elastic'
+		when abs(percent_order_count/nullif(percent_avg_price, 0)) < 1 then 'inelastic'
+	end as sensitivity
+from percent
+where percent_order_count is not null 
+	and percent_avg_price is not null
+	and abs(percent_avg_price) > 0.02
+	and previous_order_count >= 10
+order by product, month 
